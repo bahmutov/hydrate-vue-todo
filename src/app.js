36e19614efa643d155a8cfeb1a9ac576b7008e08
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-14 17:34:50 -0500

    initial

---
+++ src/app.js
@@ -0,0 +1,30 @@
+new Vue({
+  el: '#app',
+  data: {
+    newTodo: '',
+    todos: [
+      { text: 'item 1' },
+      { text: 'item 2' },
+      { text: 'item 3' },
+      { text: 'item 4' },
+      { text: 'item 5' },
+      { text: 'item 6' },
+      { text: 'item 7' },
+      { text: 'item 8' },
+      { text: 'item 9' },
+      { text: 'item 10' }
+    ]
+  },
+  methods: {
+    addTodo: function () {
+      var text = this.newTodo.trim()
+      if (text) {
+        this.todos.push({ text: text })
+        this.newTodo = ''
+      }
+    },
+    removeTodo: function (index) {
+      this.todos.splice(index, 1)
+    }
+  }
+})
